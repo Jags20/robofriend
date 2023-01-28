a28960be86bf451dc3b8e7358d56a73756a21734
Jags <jagsmk2.0@gmail.com>
2023-01-28 16:57:42 +0530

    initial changes

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Aditya',
+    username: 'Asley' ,
+    email: 'asley@april.biz'
+},
+{
+    id: 2,
+    name: 'Samar',
+    username: 'Bsley' ,
+    email: 'Bsley@april.biz'
+},
+{
+    id: 3,
+    name: 'Jags',
+    username: 'Csley' ,
+    email: 'Csley@april.biz'
+},
+{
+    id: 4,
+    name: 'Sum',
+    username: 'Dsley' ,
+    email: 'dsley@april.biz'
+},
+{
+    id: 5,
+    name: 'Maria',
+    username: 'Esley' ,
+    email: 'esley@april.biz'
+},
+{
+    id: 6,
+    name: 'Liza',
+    username: 'Fsley' ,
+    email: 'fsley@april.biz'
+},
+{
+    id: 7,
+    name: 'Shark',
+    username: 'Gsley' ,
+    email: 'gsley@april.biz'
+},
+{
+    id: 8,
+    name: 'Pall',
+    username: 'Hsley' ,
+    email: 'hsley@april.biz'
+},
+{
+    id: '9',
+    name: 'Lyf',
+    username: 'Isley' ,
+    email: 'Isley@april.biz'
+},
+{
+    id: '10',
+    name: 'Luv',
+    username: 'Jsley' ,
+    email: 'Jsley@april.biz'
+}
+] 
