579daeb5769896db8dbca8f878104dc19b91f595
Jags <jagsmk2.0@gmail.com>
2023-02-11 21:35:57 +0530

    added usestate

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
@@ -6,29 +6,39 @@ import Scroll from "../components/Scroll";
 import "./App.css";
 
 
-class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
+// class App extends Component {
+  const App = () => {
 
-    }
-  }
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(Response => Response.json())
-    .then(users=> this.setState({robots: users}));
-  }
+    useEffect(() =>{
+          fetch('https://jsonplaceholder.typicode.com/users')
+            .then(Response => Response.json())
+            .then(users=> setRobots( users));
+      },[])
 
-  onSearchChange =(event) => {
+  // constructor(){
+  //   super()
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: ''
+
+  //   }
+  // }
+
+  // componentDidMount(){
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //   .then(Response => Response.json())
+  //   .then(users=> this.setState({robots: users}));
+  // }
+
+  const onSearchChange =(event) => {
     // console.log(event.target.value);
-    this.setState({searchfield: event.target.value})
+    setSearchfield(event.target.value)
   }
 
-  render() {
-   const {robots ,searchfield} = this.state
+  {
    const filteredRobots =robots.filter(robot =>{
     return robot.name.toLowerCase().includes(searchfield.toLowerCase())
    })
@@ -37,7 +47,7 @@ class App extends Component {
    (
       <div className="tc">
           <h1 className="f2">Robofriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
           <CardList robots={filteredRobots}/>
           </Scroll>
